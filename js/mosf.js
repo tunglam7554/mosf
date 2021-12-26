4f5b2d328fc2517523de225102d04e9197f5aa12
tulaisme <tunglam7554@gmail.com>
2021-12-26 15:10:14 +0700

    form-control

--- js/mosf.js
+++ js/mosf.js
@@ -1,18 +1,26 @@
-$('document').ready(function() {
-    $('.btn-target').on('click', function() {
-        var popup = $(this).attr('target');
+$("document").ready(function () {
+    $(".btn-target").on("click", function () {
+        var popup = $(this).attr("target");
         if ($(popup).hasClass("show")) {
             $(popup).removeClass("show");
             $(".overlay").removeClass("show");
         } else {
             $(popup).addClass("show");
-            $('.overlay').attr('target', popup);
+            $(".overlay").attr("target", popup);
             $(".overlay").addClass("show");
         }
-    })
-    $('.overlay').on('click', function() {
-        var popup = $(this).attr('target');
+    });
+    $(".overlay").on("click", function () {
+        var popup = $(this).attr("target");
         $(popup).removeClass("show");
         $(".overlay").removeClass("show");
-    })
-})
+    });
+    $("input[type=file]").change(function () {
+        var filename = "";
+        for (let index = 0; index < this.files.length; index++) {
+            console.log(this.files[index]);
+            filename += this.files[index].name + "; ";
+        }
+        $(this).parent(".form-upload").children("span").text(filename);
+    });
+});
